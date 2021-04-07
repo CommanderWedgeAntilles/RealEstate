7b80db3a192f0db0bcedd997f9f9b2c70fbaeabe
CommanderWedgeAntilles <fli12003@byui.edu>
2021-04-07 12:23:53 -0600

    Model hookup going

--- model.py
+++ model.py
@@ -6,6 +6,10 @@ import json
 
 model = keras.models.load_model('model')
 
+def write_json(data,filename='searches.json'):
+    with open('searches.json','w') as f:
+        json.dump(data, f, indent=4)
+
 with open('searches.json') as json_file:
     data = json.load(json_file)
 
@@ -14,10 +18,11 @@ with open('searches.json') as json_file:
 test_data = np.array([data['current']['bedrooms'], data['current']['bathrooms'], data['current']['floors'], data['current']['sqft']])
  
 propVal = (model.predict(test_data.reshape(1,4), batch_size=1))
+propVal = (propVal*.01)
 print(propVal)
 
 
-mortgage = propVal*0.4
+mortgage = propVal*0.2
 #loan estimate (30 year 3.25% rate)
 yearLoan = ((mortgage*0.0325)*12)
 #tax estimate (0.930%)
@@ -27,6 +32,17 @@ closing = (propVal*0.035)
 annualTotal = yearLoan + yearTax + closing
 
 print(annualTotal)
+
+temp = data['current']
+x={
+    "value":propVal,
+    "loan":yearLoan,
+    "tax":yearTax,
+    "total":annualTotal
+}
+data['current'].update(x)
+print(data)
+write_json(data)
 
 json_config = model.to_json()
 new_model = keras.models.model_from_json(json_config)
