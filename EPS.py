93147d9ccd1f8563824aa9c7a0ed21b76a86ee11
CommanderWedgeAntilles <fli12003@byui.edu>
2021-04-07 00:41:13 -0600

    Pss broke EPS is fine

--- EPS.py
+++ EPS.py
@@ -1,2 +1,43 @@
 import PySimpleGUI as sg
 import os.path
+import json
+
+
+
+left=[
+    [sg.Text("Name: "), sg.In()],
+    [sg.Text("Address: "), sg.In()],
+    [sg.Text("Bedrooms: "), sg.In()],
+    [sg.Text("Bathrooms: "), sg.In()],
+    [sg.Text("Floors: "), sg.In()],
+    [sg.Text("Square Feet: "), sg.In()]
+    ]
+right=[[sg.Text("Additional Info")],[sg.In(default_text='Extra Info about Property', size=(100,300))]]
+
+
+
+
+
+layout = [
+    [sg.Button("Back"), sg.Button("Home")],
+    [sg.Text("Property Information for Evaluation")],
+    [sg.Column(left, element_justification='l'),sg.Column(right, element_justification='c')],
+    [sg.Button("Submit")]
+]
+
+window=sg.Window("Property Evaluation Application",layout)
+
+
+while True:
+    event,values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+    elif event == "Back":
+        break
+    elif event == "Home":
+        break
+    elif event == "Submit":
+        break
+window.close()
+
--- PSS.py
+++ PSS.py
@@ -1,2 +1,17 @@
 import PySimpleGUI as sg
-import os.path
+
+
+layout = [[sg.Text("Processing Property Evaluation")],
+    [sg.ProgressBar(max_value=1000, orientation='h', bar_color="red",key="progressbar")],
+    [sg.Cancel()]]
+
+window=sg.Window("Property Evaluation Progress",layout)
+progress_bar = window['progressbar']
+
+for i in range(0, 1000):
+    event, values = window.read()
+    if event == 'Cancel' or event == sg.WIN_CLOSED:
+        break
+    else:
+        progress_bar.UpdateBar(i+1)
+window.close()
