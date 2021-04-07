c55a703b9ebe65580e4ae09601be3924b7bfdc5b
CommanderWedgeAntilles <fli12003@byui.edu>
2021-04-07 11:46:50 -0600

    ITS WOOOORKING

--- model.py
+++ model.py
@@ -1,15 +1,32 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+import json
 
 
-model = keral.models.load_model('senior_forever.ipynb')
+model = keras.models.load_model('model')
 
 with open('searches.json') as json_file:
     data = json.load(json_file)
 
-np.testing.assert_allclose(model.predict(data['current']['bedrooms'], data['current']['bathrooms'], data['current']['floors'], data['current']['sqft']))
+#np.testing.assert_allclose(model.predict((data['current']['bedrooms'], data['current']['bathrooms'], data['current']['floors'], data['current']['sqft']), batch_size=1))
 
-# test_data = np.array([5,    2.50,   2910,   1.0])
+test_data = np.array([data['current']['bedrooms'], data['current']['bathrooms'], data['current']['floors'], data['current']['sqft']])
  
-# print(model.predict(test_data.reshape(1,4), batch_size=1))
+propVal = (model.predict(test_data.reshape(1,4), batch_size=1))
+print(propVal)
+
+
+mortgage = propVal*0.4
+#loan estimate (30 year 3.25% rate)
+yearLoan = ((mortgage*0.0325)*12)
+#tax estimate (0.930%)
+yearTax = (propVal*0.00930)
+#total cost estimate (add closing cost (0.035))
+closing = (propVal*0.035)
+annualTotal = yearLoan + yearTax + closing
+
+print(annualTotal)
+
+json_config = model.to_json()
+new_model = keras.models.model_from_json(json_config)
